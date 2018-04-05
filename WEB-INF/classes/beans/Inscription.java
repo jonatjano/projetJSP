02fd3c457afce049b74e06ff3eb24af196a104d7
totatoto <adam.bernouy@orange.fr>
2018-04-05 08:29:58 +0200

    inscription Ok

---
+++ WEB-INF/classes/beans/Inscription.java
@@ -0,0 +1,52 @@
+package pei;
+public class Inscription
+{
+	int idp;
+	int ide;
+	String categTarif;
+	
+	public int getIdp()
+	{
+		return idp;
+	}
+	
+	public int getIde()
+	{
+		return ide;
+	}
+	
+	public int getCategTarif()
+	{
+		return categTarif;
+	}
+	
+	public void setIdp(int idp)
+	{
+		this.idp = idp;
+	}
+	
+	public void setIde(int ide)
+	{
+		this.ide=ide;
+	}
+	
+	public void setCategTarif(int categTarif)
+	{
+		this.categTarif=categTarif;
+	}
+	
+	public Inscription()
+	{}
+	
+	public Inscription(int idp, int ide,  String categTarif)
+	{
+		this.idp = idp;
+		this.ide = ide;
+		this.categTarif = categTarif;
+	}
+	
+	public String toString()
+	{
+		return idp + "," + ide + "," + categTarif;
+	}
+}
